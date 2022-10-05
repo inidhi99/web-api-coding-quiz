011bbb2fbba40376c98af6be11f538052c899dd5
nidhi <inidhi99@gmail.com>
2022-10-05 18:15:37 -0400

    modified html, css and JS

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 
 
+var questionelement = document.getElementById('questions'); // handle 
+var optionsdiv = document.getElementsByClassName('options') [0];
+var options1element = document.getElementById('option1');
+var options2element = document.getElementById('option2');
+var options3element = document.getElementById('option3');
+var options4element = document.getElementById('option4');
+var optionsarray =[ options1element, options2element, options3element, options4element]; 
+var result = document.getElementById('result');
+var timeLeft = document.getElementById("timer");
+var questionNumber =0;
+var score = 0; 
+var quizElement = document.getElementById('quiz'); 
+var nameDiv = document.getElementById('name-div');
+var scoreElement = document.getElementById('score');
+var button = document.getElementById('name-submit');
+var nameElement = document.getElementById('name');
+var scoresElement= document.getElementById('highscores');
+var highscores = JSON.parse(localStorage.getItem('scores'));
 
-
-
-
-var Questions = [
+var questions = [
     {
         question: "Questions 1 : Commonly used data types DO NOT include:",
         options: ["a. strings", "b. booleans", "c. alerts", "d. numbers"],
@@ -19,22 +34,94 @@ var Questions = [
 
     {
         question: "Questions 3 : Arrays in JavaScript can be used to store ____",
-        choices: ["a. numbers and string", "b. other arrays", "c. booleans", "d. all the above()"],
+        options: ["a. numbers and string", "b. other arrays", "c. booleans", "d. all the above()"],
         answer: "d"
     },
     {
         question: "Questions 4 : String values must be enclosed within _____ when being assigned to variables.",
-        choices: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
+        options: ["a. commas", "b. curly brackets", "c. quotes", "d. parenthesis"],
         answer: "c"
     },
 
 ];
 
-var timeLeft = document.getElementById("timer");
+
+function loadQuestion (questionNumber) {
+    questionelement.textContent = questions[questionNumber].question;
+
+    for ( var i =0; i <4; i++) {
+        optionsarray[i].textContent =questions[questionNumber].options[i];
+    } 
+    
+}
+
+function loadScoreBoard (){
+    questionelement.remove();
+    optionsdiv.remove();
+    timeLeft.remove();
+    result.remove();
+
+    scoreElement.textContent = score;
+    nameDiv.style.display= "block";
+    loadHighscores(); 
+};
+
+function loadHighscores (){
+
+    var highscores= JSON.parse(localStorage.getItem('scores'))
+    var scoresList = document.createElement("ul");
+
+    for (var highscore of highscores) {
+        var lineElement = document.createElement('li');
+        lineElement.textContent = `Name: ${highscore.name}, Score: ${highscore.score}`;
+        scoresList.appendChild(lineElement);
+    }
+
+    scoresElement.appendChild(scoresList);
+}
+
+
+optionsdiv.addEventListener( 'click', function(event) {
+   var correctAnswer = questions[questionNumber].answer 
+   var answerChoice = event.target.textContent[0];
+    if ( correctAnswer === answerChoice){
+        result.textContent = 'correct';
+        score+=25; 
+
+    } else {
+        result.textContent = 'wrong';
+    }
+
+    questionNumber++ 
+
+    if (questionNumber < questions.length ) {
+        loadQuestion(questionNumber); 
+
+    } else {
+        loadScoreBoard ();
+    }
+});
+
+
+button.addEventListener ('click', function(){
+    highscores.push({ 
+        name: nameElement.value,
+        score: score
+    })
+
+    localStorage.setItem('scores', JSON.stringify(highscores));
+    scoresElement.children[0].remove();  loadHighscores();
+});
+
+
+
+
+
+
+
 
 var secondsLeft = 60;
 var questionCount = 1;
-
 
 function setTime() {
         
@@ -42,10 +129,15 @@ function setTime() {
           secondsLeft--;
           timeLeft.textContent = "Time: " + secondsLeft + "S";
     
-            if (secondsLeft <= 0 || questionCount >= questionSource.length +1){
+            if (secondsLeft <= 0 || questionCount >= questions.length +1){
                 clearInterval(timerInterval);
-                finish.textContent = "Game Over!";
-                gameOver();
+                timeLeft.textContent = "Game Over!";
+            
             }
     }, 1000);
 }
+setTime();
+loadQuestion(0);
+
+
+
